80635bbb30ad4598083cfc99a41e9c62c21e41d6
Matthew Dicataldo <matthewdicataldo@gmail.com>
2025-06-16 21:21:10 -0700

    Re-export the `fingerprint` module

--- beat.zig
+++ beat.zig
@@ -19,6 +19,7 @@ pub const scheduler = @import("src/scheduler.zig");
 pub const pcall = @import("src/pcall.zig");
 pub const coz = @import("src/coz.zig");
 pub const testing = @import("src/testing.zig");
+pub const fingerprint = @import("src/fingerprint.zig");
 
 // Import core for main types
 const core = @import("src/core.zig");
